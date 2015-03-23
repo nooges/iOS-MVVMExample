8c3a7983f3f68abf12efaee88057cb280dad8bb4
Danny Nguyen <danny@80pct.com>
2015-03-23 13:16:19 -0400

    Initial Commit

---
+++ MVVMExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMExample
+//
+//  Created by Danny Nguyen on 3/23/15.
+//  Copyright (c) 2015 freedom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVVMExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVMExample
+//
+//  Created by Danny Nguyen on 3/23/15.
+//  Copyright (c) 2015 freedom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
